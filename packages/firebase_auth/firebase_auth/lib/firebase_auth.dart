0035f5dd015a3803ba3e4fa335884a04380f8fbd
u229751 <dominik.mosberger@sbb.ch>
2020-05-19 20:28:02 +0200

    add microsoft auth provider

--- packages/firebase_auth/firebase_auth/lib/firebase_auth.dart
+++ packages/firebase_auth/firebase_auth/lib/firebase_auth.dart
@@ -26,6 +26,7 @@ part 'src/auth_provider/google_auth_provider.dart';
 part 'src/auth_provider/phone_auth_provider.dart';
 part 'src/auth_provider/twitter_auth_provider.dart';
 part 'src/auth_provider/oauth_auth_provider.dart';
+part 'src/auth_provider/microsoft_auth_provider.dart';
 part 'src/additional_user_info.dart';
 part 'src/auth_result.dart';
 part 'src/firebase_auth.dart';
